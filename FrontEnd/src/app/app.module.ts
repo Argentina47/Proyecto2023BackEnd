6d0dbf2236f7c354875ef8d8cae14b452d62240f
Argentina47 <100891285+Argentina47@users.noreply.github.com>
2022-09-07 00:18:24 -0300

    componentes

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SocialComponent } from './components/social/social.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HysComponent,
+    ProyectosComponent,
+    SocialComponent,
+    FooterComponent,
+    LogoApComponent
   ],
   imports: [
     BrowserModule,
