312a2d79d19f2f358e24dd11cc5afa7d7f7ceffc
Argentina47 <100891285+Argentina47@users.noreply.github.com>
2022-12-09 05:29:50 -0300

    ..

---
+++ BackEnd/src/main/java/com/proyecto2023/soniasoacha/Security/jwt/JwtTokenFliter.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyecto2023.soniasoacha.Security.jwt;
+
+import com.proyecto2023.soniasoacha.Security.Service.UserDetailsImpl;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtTokenFliter extends OncePerRequestFilter {
+
+    private final static Logger logger = LoggerFactory.getLogger(com.proyecto2023.soniasoacha.Security.jwt.JwtProvider.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    UserDetailsImpl userDetailsServiceImpl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            if (token != null && jwtProvider.validateToken(token)) {
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                } 
+            
+
+        catch (Exception e) {
+            logger.error("Falla en el metodo")
+        }
+
+    }
+
+}
